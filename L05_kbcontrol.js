3281f8e1bbe2c8c93f790ce3ac63942cdc18204f
guekxy <guekxy@gmail.com>
2026-08-16 03:25:19 +0000

    Aug 16, 2026, 3:25 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,1 +1,31 @@
-// write your codes here
+
+
+let xpos = 0;
+let ypos = 0;
+size = 
+xpos = width/2
+ypos = height/2
+
+function draw(){
+    background('sky blue');
+    fill (colour);
+    circle(xpos,ypos,size);
+
+}
+if (keyIsDown(RIGHT_ARROW)) {
+    xpos = xpos +2;
+
+}
+if (keyIsDown(LEFT_ARROW)) {
+    xpos = xpos -2;
+
+}
+
+if (keyIsDown(UP_ARROW)) {
+    ypos = ypos - 2;
+}
+
+if (keyIsDown(DOWN_ARROW)) {
+    ypos = ypos + 2;
+
+}
