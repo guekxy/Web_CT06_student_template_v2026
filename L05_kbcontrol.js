fc4f140262404972aa7140280e9056ec1bb23f9c
guekxy <guekxy@gmail.com>
2026-08-16 03:35:29 +0000

    Aug 16, 2026, 3:35 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -7,7 +7,7 @@ ypos = height/2
 
 
 let colour = ('blue')
-
+}
 
 function draw(){
     background('sky blue');
@@ -31,6 +31,5 @@ if (keyIsDown(UP_ARROW)) {
 if (keyIsDown(DOWN_ARROW)) {
     ypos = ypos + 2;
 
-}
 }
 }
