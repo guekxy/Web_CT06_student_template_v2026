d7c05e395d62e366566e2757b58f7d771b4fdb8b
guekxy <guekxy@gmail.com>
2026-08-23 04:21:13 +0000

    Aug 23, 2026, 4:21 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -45,7 +45,7 @@ function draw() {
         }
         else if (keyCode === DOWN_ARROW) {
             ballSpeedX = ballSpeedX * 0.67;
-            ballSpeedY = ballSpeedYm
+            ballSpeedY = ballSpeedY * 0.67;
         }
 
 }
