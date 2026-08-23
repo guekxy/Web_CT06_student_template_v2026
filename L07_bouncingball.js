6ce81fa7a5ca4acb980df765a16b253f85bfe5ef
guekxy <guekxy@gmail.com>
2026-08-23 04:04:20 +0000

    Aug 23, 2026, 4:04 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -24,15 +24,15 @@ function draw() {
     ballY = ballY + ballSpeedY;
     if (ballX > width) {
         ballSpeedX = -1 * ballSpeedX;
-        fill (random(0,255)),(random(0,255)),(random(0,55));
+        fill (random(0,255)),(random(0,255)),(random(10,55));
     }
     if (ballX < 0) {
         ballSpeedX = -1 * ballSpeedX;
-        fill (random(0,255)),(random(0,255)),(random(0,55));
+        fill (random(0,255)),(random(0,255)),(random(10,55));
     }
     if (ballY > height ){
         ballSpeedY = -1 * ballSpeedY;
-        fill (random(0,255)),(random(0,255)),(random(0,55));
+        fill (random(0,255)),(random(0,255)),(random(10,55));
     }
     if (ballY < 0) {
         ballSpeedY = -1 * ballSpeedY;
