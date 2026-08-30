87477ac3e7c4bfbac3128b9e51c25b81987c80c8
guekxy <guekxy@gmail.com>
2026-08-30 04:34:27 +0000

    Aug 30, 2026, 4:34 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -6,6 +6,23 @@ function setup() {
   
 function draw() {
   // Recap 1: Repeating Circles
+  fill("lime");
+
+  let circleDiameter = 50;
+  let circleY = 50;
+
+  for (let num=1; num<=5; num++) {
+    circle(50*num,circleY,circleDiameter);
+    circleY = circleY + 50;
+    print("i will not sling mud at my friend");
+    print(width);
+    print(height);
+  }
+
+  // circle(150,50,50);
+  // circle(250,50,50);
+  // circle(350,50,50);
+  // circle(450,50,50);
 
   // Task 1: Colour Gradient
 
