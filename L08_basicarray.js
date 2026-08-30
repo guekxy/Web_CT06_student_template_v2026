530da703b7fed061758ee4e643c44d0bc966d484
guekxy <guekxy@gmail.com>
2026-08-30 03:23:46 +0000

    Aug 30, 2026, 3:23 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
     background(220);
     textSize(64);
-    text("hello everybody", 100, height/2);
+    text("hello everybody", 100, ypos);
     text("second line", 100, height/2 + 18);
     ypos = -0.67
     text("today")
