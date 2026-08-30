efa95d0418e40f21a88260d18156d942ef57dc70
guekxy <guekxy@gmail.com>
2026-08-30 03:28:06 +0000

    Aug 30, 2026, 3:28 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,7 +10,7 @@ function draw() {
     textSize(64);
     text("hello everybody", 100, ypos);
     text("second line", 100, ypos + 18);
-    text("ethan wong joined our sunday class!")
+    text("ethan wong joined our sunday class!",100,)
     ypos = ypos -0.67
    
 
