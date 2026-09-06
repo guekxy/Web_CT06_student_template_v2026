6eae0781f4051cf526bfb76c935bc719d7b48de3
guekxy <guekxy@gmail.com>
2026-09-06 01:48:10 +0000

    Sep 6, 2026, 1:48 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -14,6 +14,7 @@ function draw() {
   for (let num=1; num<=5; num++) {
     circle(50*num,circleY,circleDiameter);
     circleY = circleY + 50;
+    fill(i)
     print("i will not sling mud at my friend");
     print(width);
     print(height);
